c847f8174595c732b97d25ce8fc8a2c793f854ca
jey0623 <jey0623@naver.com>
2021-11-14 21:07:10 +0900

    211114_category

--- board/views.py
+++ board/views.py
@@ -13,16 +13,7 @@ from .models import Category, Board
 from .serializer import *
 from django.http import HttpResponse, JsonResponse
 
-class CategoryViewSet(APIView):
-    def get(self, request, format=None):
-        queryset=Category.objects.all()#카테고리 모델을 모두 부른다.
-        serializer_class = CategorySerializer
 
-        queryset = self.get_queryset()
-        serializer_class = self.get_serializer_class()
-
-        serializer = serializer_class(queryset, many=True)
-        return HttpResponse(serializer.data, status=200)
 
 # # board.html 페이지를 부르는 index 함수
 # def index(request):
@@ -41,7 +32,6 @@ class CategoryViewSet(APIView):
 #     post = Board.objects.get(pk=pk)
 #     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
 #     return HttpResponse({'post':post},status=200)
-
 
 # 새로운 게시글 작성하는 함수
 def new_post(request):
@@ -78,5 +68,16 @@ class BoardListView(generics.ListAPIView):
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
+
+        return HttpResponse(json.dumps(serializer.data, ensure_ascii=False, indent='\t'), status=200)
+
+class CategoryViewSet(generics.ListAPIView):
+    queryset = Category.objects.all()  # 카테고리 모델을 모두 부른다.
+    serializer_class = CategorySerializer
+
+    def get(self, request, format=None):
+        queryset = self.get_queryset()
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(queryset, many=True)
 
         return HttpResponse(json.dumps(serializer.data, ensure_ascii=False, indent='\t'), status=200)
