8abbb9c5f98fb04041a25396cc39f01ffa4d93b8
jey0623 <jey0623@naver.com>
2022-03-02 21:53:43 +0900

    deploy

--- deliveryFriend/wsgi.py
+++ deliveryFriend/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from gunicorn import app
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deliveryFriend.settings')
 
 application = get_wsgi_application()
+
+port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=port, debug=True)
+
